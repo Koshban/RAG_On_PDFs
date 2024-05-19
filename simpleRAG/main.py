afb028394140eb9d50509c2597959148da81ec62
Kaushik Banerjee <96236630+Koshban@users.noreply.github.com>
2024-05-19 19:24:00 +0800

    Adding Embeddings

--- simpleRAG/main.py
+++ simpleRAG/main.py
@@ -1,19 +1,25 @@
 """ Based on using Vector DB and RAG to extract relevant data from the Facts.txt file and asnwer quesries based on that data"""
 from dotenv import load_dotenv, find_dotenv
 from langchain_openai import ChatOpenAI
-from langchain.prompts import PromptTemplate
-from langchain.document_loaders import TextLoader
-from langchain.chains import LLMChain, SequentialChain
+#from langchain.prompts import PromptTemplate
+from langchain_community.document_loaders import TextLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+#from langchain.chains import LLMChain, SequentialChain
 import argparse
 import os
 
 def setup_env(file: str):
     load_dotenv(find_dotenv())
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    loader = TextLoader(f'{BASE_DIR}\\Facts.txt')
+    embeddings = OpenAIEmbeddings()
+    emb = embeddings.embed_query("My name is Kaushik")
+    text_splitter = CharacterTextSplitter(separator="\n", chunk_size=200, chunk_overlap=0)
+    loader = TextLoader(f'{BASE_DIR}\\{file}')
     llm = ChatOpenAI()
-    docs = loader.load()
-    #print(docs)
+    docs = loader.load_and_split(text_splitter=text_splitter)
+    # for d in docs:
+    #     print(d.page_content, "\n")
 
 
 def get_args() -> str:
